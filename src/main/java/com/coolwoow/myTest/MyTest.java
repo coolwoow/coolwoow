83e562273ea834b52f51b6bdb2ae4d9332aacf46
coolwoow <1301295260@qq.com>
2022-07-10 19:04:20 +0800

    添加文件

---
+++ src/main/java/com/coolwoow/myTest/MyTest.java
@@ -0,0 +1,11 @@
+package com.coolwoow.myTest;
+
+/**
+ * @author coolwoow
+ * @create 2022-07-10
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
